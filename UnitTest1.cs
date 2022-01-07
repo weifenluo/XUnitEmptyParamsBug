c68501f46ebca08502f4c28732b0885207c8c613
Weifen Luo <weifenluo@yahoo.com>
2022-01-08 07:40:55 +0800

    Add passing an empty array.

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -5,7 +5,9 @@ namespace XUnitEmptyParamsBug
     public class UnitTest1
     {
         [Theory]
-        [InlineData]
+        // This does not work either
+        // [InlineData]
+        [InlineData(new object[] {})]
         public void Test1(params string[] values)
         {
             Assert.Empty(values);
